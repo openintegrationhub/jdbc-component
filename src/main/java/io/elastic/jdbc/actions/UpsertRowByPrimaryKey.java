2798e6c07e743e3e8b87a223fae63300184f53fa
Olha Virolainen <olga.shukel@gmail.com>
2018-08-13 10:26:04 +0300

    UpsertRowByPrimaryKey action migrate to sailor v2

--- src/main/java/io/elastic/jdbc/actions/UpsertRowByPrimaryKey.java
+++ src/main/java/io/elastic/jdbc/actions/UpsertRowByPrimaryKey.java
@@ -6,12 +6,13 @@ import io.elastic.api.Module;
 import io.elastic.jdbc.QueryBuilders.Query;
 import io.elastic.jdbc.QueryFactory;
 import io.elastic.jdbc.Utils;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import io.elastic.jdbc.Engines;
+
 import javax.json.Json;
-import javax.json.JsonArray;
 import javax.json.JsonObject;
-import javax.json.JsonString;
+import javax.json.JsonValue;
+
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,58 +24,105 @@ public class UpsertRowByPrimaryKey implements Module {
   private static final Logger logger = LoggerFactory.getLogger(UpsertRowByPrimaryKey.class);
   private static final String PROPERTY_DB_ENGINE = "dbEngine";
   private static final String PROPERTY_TABLE_NAME = "tableName";
-  private static final String PROPERTY_ID_COLUMN = "idColumn";
-
-  private Connection connection = null;
-
-  /* TODO: UpsertRowByPrimaryKey action migrate to sailor v2 */
 
   @Override
   public void execute(ExecutionParameters parameters) {
     final JsonObject configuration = parameters.getConfiguration();
     final JsonObject body = parameters.getMessage().getBody();
-    if (!configuration.has(PROPERTY_TABLE_NAME) || configuration.get(PROPERTY_TABLE_NAME)
-        .isJsonNull() || configuration.get(PROPERTY_TABLE_NAME).getAsString().isEmpty()) {
+    JsonObject snapshot = parameters.getSnapshot();
+    String tableName = "";
+    String dbEngine = "";
+    String schemaName = "";
+    ResultSet rs = null;
+    String primaryKey = "";
+    StringBuilder primaryValue = new StringBuilder();
+    Integer primaryKeysCount = 0;
+
+    if (configuration.containsKey(PROPERTY_TABLE_NAME)
+            && Utils.getNonNullString(configuration, PROPERTY_TABLE_NAME).length() != 0) {
+      tableName = configuration.getString(PROPERTY_TABLE_NAME);
+    } else if (snapshot.containsKey(PROPERTY_TABLE_NAME)
+            && Utils.getNonNullString(snapshot, PROPERTY_TABLE_NAME).length() != 0) {
+      tableName = snapshot.getString(PROPERTY_TABLE_NAME);
+    } else {
       throw new RuntimeException("Table name is required field");
     }
-    if (!configuration.has(PROPERTY_ID_COLUMN) || configuration.get(PROPERTY_ID_COLUMN).isJsonNull()
-        || configuration.get("idColumn").getAsString().isEmpty()) {
-      throw new RuntimeException("ID column is required field");
+
+    if (Utils.getNonNullString(configuration, PROPERTY_DB_ENGINE).length() != 0) {
+      dbEngine = configuration.getString(PROPERTY_DB_ENGINE);
+    } else if (Utils.getNonNullString(snapshot, PROPERTY_DB_ENGINE).length() != 0) {
+      dbEngine = snapshot.getString(PROPERTY_DB_ENGINE);
+    } else {
+      throw new RuntimeException("DB Engine is required field");
     }
-    String tableName = configuration.get(PROPERTY_TABLE_NAME).getAsString();
-    String idColumn = configuration.get(PROPERTY_ID_COLUMN).getAsString();
-    String idColumnValue = null;
-    if (!(!body.has(idColumn) || body.get(idColumn).isJsonNull() || body.get(idColumn).getAsString()
-        .isEmpty())) {
-      idColumnValue = body.get(idColumn).getAsString();
-    }
-    logger.info("ID column value: {}", idColumnValue);
-    String db = configuration.get(PROPERTY_DB_ENGINE).getAsString();
-    boolean isOracle = db.equals(Engines.ORACLE.name().toLowerCase());
+
+    logger.info("Executing lookup primary key");
+    boolean isOracle = dbEngine.equals(Engines.ORACLE.name().toLowerCase());
+    Connection connection = Utils.getConnection(configuration);
+
     try {
-      connection = Utils.getConnection(configuration);
-      Map<String, String> columnTypes = Utils.getColumnTypes(connection, isOracle, tableName);
-      logger.info("Detected column types: " + columnTypes);
-      QueryFactory queryFactory = new QueryFactory();
-      Query query = queryFactory.getQuery(PROPERTY_DB_ENGINE);
-      query.from(tableName).lookup(idColumn, idColumnValue);
-      if (query.executeRecordExists(connection)) {
-        query.executeUpdate(connection, tableName, idColumn, idColumnValue, body);
-      } else {
-        query.executeInsert(connection, tableName, body);
+      DatabaseMetaData dbMetaData = connection.getMetaData();
+      if (tableName.contains(".")) {
+        schemaName =
+                (isOracle) ? tableName.split("\\.")[0].toUpperCase() : tableName.split("\\.")[0];
+        tableName =
+                (isOracle) ? tableName.split("\\.")[1].toUpperCase() : tableName.split("\\.")[1];
       }
-      this.getEventEmitter().emitData(new Message.Builder().body(body).build());
+      rs = dbMetaData
+              .getPrimaryKeys(null, ((isOracle && !schemaName.isEmpty()) ? schemaName : null),
+                      tableName);
+      while (rs.next()) {
+        primaryKey=rs.getString("COLUMN_NAME");
+        primaryKeysCount++;
+      }
+      if (primaryKeysCount == 1) {
+        logger.info("Executing upsert row by primary key action");
+        for (Map.Entry<String, JsonValue> entry : body.entrySet()) {
+          if (entry.getKey().equals(primaryKey)) {
+            logger.info("Primary key{} = {}", entry.getKey(), entry.getValue());
+            primaryValue.append(entry.getValue());
+          }
+        }
+        Utils.columnTypes = Utils.getColumnTypes(connection, isOracle, tableName);
+        logger.info("Detected column types: " + Utils.columnTypes);
+        Map<String, String> columnTypes = Utils.getColumnTypes(connection, isOracle, tableName);
+        QueryFactory queryFactory = new QueryFactory();
+        Query query = queryFactory.getQuery(dbEngine);
+        logger.info("Lookup parameters: {} = {}", primaryKey.toString(), primaryValue.toString());
+        query.from(tableName).lookup(primaryKey.toString(), primaryValue.toString());
+        if (query.executeRecordExists(connection)) {
+          logger.info("Update parameters: {} = {}", primaryKey.toString(), primaryValue.toString());
+          query.executeUpdate(connection, tableName, primaryKey.toString(), primaryValue.toString(), body);
+        } else {
+          logger.info("Insert parameters: {} = {}", primaryKey.toString(), primaryValue.toString());
+          query.executeInsert(connection, tableName, body);
+        }
+        logger.info("Emit data= {}", body.toString());
+        parameters.getEventEmitter().emitData(new Message.Builder().body(body).build());
+        snapshot = Json.createObjectBuilder().add(PROPERTY_TABLE_NAME, tableName).build();
+        logger.info("Emitting new snapshot {}", snapshot.toString());
+        parameters.getEventEmitter().emitSnapshot(snapshot);
+      } else {
+        logger.error("Error: Should be one Primary Key");
+        throw new IllegalStateException("Should be one Primary Key");
+      }
     } catch (SQLException e) {
       throw new RuntimeException(e);
     } finally {
+      if (rs != null) {
+        try {
+          rs.close();
+        } catch (SQLException e) {
+          System.out.println("Failed to close result set" + e.toString());
+        }
+      }
       if (connection != null) {
         try {
           connection.close();
         } catch (SQLException e) {
-          logger.error(e.toString());
+          System.out.println("Failed to close connection" + e.toString());
         }
       }
     }
   }
-
 }
