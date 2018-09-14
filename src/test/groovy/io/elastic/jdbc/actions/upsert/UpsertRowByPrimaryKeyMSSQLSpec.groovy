67d39b98885e9c311d379c8b7fab10784e483e88
Olha Virolainen <olga.shukel@gmail.com>
2018-09-14 15:06:30 +0300

    Ignore test

--- src/test/groovy/io/elastic/jdbc/actions/upsert/UpsertRowByPrimaryKeyMSSQLSpec.groovy
+++ src/test/groovy/io/elastic/jdbc/actions/upsert/UpsertRowByPrimaryKeyMSSQLSpec.groovy
@@ -12,7 +12,7 @@ import java.sql.Connection
 import java.sql.DriverManager
 import java.sql.ResultSet
 
-//@Ignore
+@Ignore
 class UpsertRowByPrimaryKeyMSSQLSpec extends Specification {
 
   @Shared
